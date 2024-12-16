fde36c0dcacb0070beabdaa9cb077fd7325294b6
Yixuan <yd271@georgetown.edu>
2024-12-17 06:12:46 +0800

    upload frontend code

---
+++ multi-user/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Comment from './component/Comment/Comment.js';
+function App() {
+  return (
+    <div className="App">
+      <Comment />
+    </div>
+  );
+}
+
+export default App;
